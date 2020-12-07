c2e4427ca03f5712130098c68f520a4402cdbd93
Mahesh Chinchure <mchinchure@falabella.cl>
2020-12-07 21:51:40 +0530

    First commit

---
+++ src/main/java/com/test/firstSpringExample/FirstSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.test.firstSpringExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringExampleApplication.class, args);
+	}
+
+}
